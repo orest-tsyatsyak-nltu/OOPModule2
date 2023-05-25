1c66e6caa0bf59b93a4706ce1307a93a0236c48b
Orest Tsiatsiak <oresttsytsyak02@gmail.com>
2023-05-25 16:24:45 +0300

    Add project files.

---
+++ Module2OOP/Product.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module2OOP
+{
+    [Serializable]
+    internal class Product : IComparable<Product>
+    {
+
+        private string _name;
+        private DateTime _expirationDate;
+        private string _manufacturerName;
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }        
+
+        public DateTime ExpirationDate
+        {
+            get { return _expirationDate; }
+            set { _expirationDate = value; }
+        }
+
+        public string ManufacturerName
+        {
+            get { return _manufacturerName; }
+            set { _manufacturerName = value; }
+        }
+
+        public Product(string name, DateTime expirationDate, string manufacturerName)
+        {
+            Name = name;
+            ExpirationDate = expirationDate;
+            ManufacturerName = manufacturerName;
+        }
+
+        public int CompareTo(Product other)
+        {
+            return string.Compare(Name, other.Name);
+        }
+
+        public override string ToString()
+        {
+            return Name + " " + ExpirationDate + " " + ManufacturerName;
+        }
+    }
+}
---
+++ Module2OOP/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module2OOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedList<Product, Product> products = new SortedList<Product, Product>();
+            Product catsFood = new Product("Well fed cat", DateTime.Now.AddMonths(6), "Cats food company");
+            products.Add(catsFood, catsFood);
+            Product dogsFood = new Product("Well fed dog", DateTime.Now.AddMonths(9), "Dogs food company");
+            products.Add(dogsFood, dogsFood);
+            Product humanFood = new Product("Best tuna", DateTime.Now.AddDays(14), "Human food company");
+            products.Add(humanFood, humanFood);
+
+            byte[] serializedProducts = SerializeToBinary(products);
+            SortedList<Product, Product> deserializedProducts = DeserializeFromBinary<SortedList<Product, Product>>(serializedProducts);
+
+            foreach (var p in deserializedProducts)
+            {
+                Console.WriteLine(p);
+            }
+            Console.ReadLine();
+        }
+
+        public static byte[] SerializeToBinary<T>(T obj)
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            using (MemoryStream stream = new MemoryStream())
+            {
+                formatter.Serialize(stream, obj);
+                return stream.ToArray();
+            }
+        }
+
+        public static T DeserializeFromBinary<T>(byte[] binaryData)
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            using (MemoryStream stream = new MemoryStream(binaryData))
+            {
+                return (T)formatter.Deserialize(stream);
+            }
+        }
+    }
+}
